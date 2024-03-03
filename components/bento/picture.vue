21117c4fb3f0336b13ef1b2ab967e7930d609a6a
simonbotte <simon.botte@icloud.com>
2024-03-03 17:52:33 +0100

    Add BentoPicture component and suricate image

---
+++ components/bento/picture.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+    src: {
+        type: String,
+        required: true,
+    },
+    alt: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="h-bento-mobile-2 row-span-2 tablet:h-bento-tablet-2 laptop:h-bento-laptop-2 rounded-2xl overflow-hidden">
+        <NuxtImg class="h-full w-full object-cover" :src="src" :alt="alt" :width="280"/>
+    </div>
+</template>
