7d0acb85a8c6060dcc55033a674626933f01e0b0
simonbotte <simon.botte@icloud.com>
2024-03-16 15:03:33 +0100

    Update grid gap in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-    <section class="grid grid-cols-2 w-full max-w-mobile px-4 m-auto gap-4 tablet:max-w-tablet tablet:grid-cols-3 laptop:max-w-laptop laptop:grid-cols-4">
+    <section class="grid grid-cols-2 w-full max-w-mobile px-4 m-auto gap-4 tablet:max-w-tablet tablet:grid-cols-3 laptop:max-w-laptop laptop:grid-cols-4 laptop:gap-8">
         <BentoHi class="col-span-2 h-bento-mobile tablet:h-bento-tablet laptop:h-bento-laptop" />
         <BentoEmpty class="col-span-1" />
         <BentoPicture src="/images/suricate.jpeg" alt="Photo d'un suricate" />
