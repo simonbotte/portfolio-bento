7f2bfa9a5f32fc0ea16d7d8c25367440fd14a28a
simonbotte <simon.botte@icloud.com>
2024-03-29 18:50:25 +0100

    Update project images in projects.vue

--- components/bento/projects.vue
+++ components/bento/projects.vue
@@ -5,8 +5,8 @@
     >
         <p class="relative z-10 text-2xl text-center font-bold tablet:text-3xl laptop:text-4xl"> Voir mes projets</p>
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex opacity-40 justify-center w-full items-center">
-            <NuxtImg src="/images/bento-project-left.png" alt="Image projet" :width="250" class="shrink-0 w-12 object-contain transition-transform duration-500 scale-125 group-hover:scale-150 tablet:w-24" />
-            <NuxtImg src="/images/bento-project-right.png" alt="Image projet" :width="150" class="shrink-0 w-8 object-contain transition-transform duration-500 group-hover:scale-125 tablet:w-16"/>
+            <NuxtImg src="/images/bento-project-left.png" alt="Image projet" :width="250" class="shrink-0 w-16 object-contain transition-transform duration-500 scale-125 group-hover:scale-150 tablet:w-24" />
+            <NuxtImg src="/images/bento-project-right.png" alt="Image projet" :width="150" class="shrink-0 w-10 object-contain transition-transform duration-500 group-hover:scale-125 tablet:w-16"/>
         </div>
     </NuxtLink>
 </template>
