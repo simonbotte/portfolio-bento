690651701ff88b08c23ffbffe3ca83ec81cbf0ac
simonbotte <simon.botte@icloud.com>
2024-03-30 09:12:57 +0100

    Refactor music.vue component

--- components/bento/music.vue
+++ components/bento/music.vue
@@ -1,14 +1,4 @@
 <script setup>
-// useHead({
-//     script: [
-//         {
-//             src: "https://js-cdn.music.apple.com/musickit/v1/musickit.js",
-//             async: true,
-//             "data-web-components": "true",
-//         },
-//     ],
-// });
-
 const token = ref("");
 const { data: jwtToken } = await useFetch("/api/security/apple-jwt", { method: "POST" });
 token.value = jwtToken.value.token;
@@ -21,24 +11,6 @@ const musicUrl = ref(`https://api.music.apple.com/v1`);
 
 const music = ref(null);
 
-// const loadMusic = async () => {
-//     console.log("MusicKit is ready");
-//     try {
-//         await MusicKit.configure({
-//             developerToken: token.value,
-//             app: {
-//                 name: "Portfolio bento",
-//                 build: "1.0.0",
-//             },
-//         });
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     // MusicKit instance is available
-//     music.value = MusicKit.getInstance();
-//     await music.value.authorize();
-// };
-
 const getRecentPlayed = async () => {
     const { data, error } = await useFetch(musicUrl.value + "/me/recent/played/tracks?limit=1", {
         headers: {
@@ -46,25 +18,22 @@ const getRecentPlayed = async () => {
             "Music-User-Token": userToken.value,
         },
         method: "GET",
+        lazy: true,
     });
+
     return data.value.data[0];
 };
 const lastPlayedMusic = await getRecentPlayed();
 const musicBento = ref(null);
 onMounted(async () => {
-    // console.log("MusicKit is ready not event");
-    // document.addEventListener("musickitloaded", async function () {
-    //     await loadMusic();
-    // });
-    // await loadMusic();
     useApparitionAnimation(musicBento.value);
 });
 </script>
 
 <template>
     <div
-    ref="musicBento"
-        class=" bg-sand-800 p-4 h-bento-mobile-1 flex flex-col gap-2 justify-between rounded-2xl overflow-hidden h-bento-mobile tablet:p-4 tablet:h-bento-tablet laptop:p-6 laptop:h-bento-laptop"
+        ref="musicBento"
+        class="bg-sand-800 p-4 h-bento-mobile-1 flex flex-col gap-2 justify-between rounded-2xl overflow-hidden h-bento-mobile tablet:p-4 tablet:h-bento-tablet laptop:p-6 laptop:h-bento-laptop"
     >
         <div class="">
             <NuxtImg
@@ -86,7 +55,7 @@ onMounted(async () => {
                             lastPlayedMusic.attributes.name
                         }}</NuxtLink>
                     </h3>
-                    <p class="text-sand-100/80 text-xs max-h-8 overflow-hidden ">
+                    <p class="text-sand-100/80 text-xs max-h-8 overflow-hidden">
                         {{ lastPlayedMusic.attributes.artistName }}
                     </p>
                 </div>
