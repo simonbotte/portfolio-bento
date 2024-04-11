9f24d0960bdf7bae4e81fe810cbf936a5025b7df
simonbotte <simon.botte@icloud.com>
2024-04-11 11:14:41 +0200

    Refactor header component to improve readability and responsiveness

--- components/app/header.vue
+++ components/app/header.vue
@@ -2,9 +2,12 @@
 const isOpen = ref(false);
 const route = useRoute();
 
-watch(() => route.fullPath, () => {
-    isOpen.value = false;
-});
+watch(
+    () => route.fullPath,
+    () => {
+        isOpen.value = false;
+    }
+);
 const toggleMenu = () => {
     isOpen.value = !isOpen.value;
 };
@@ -12,36 +15,37 @@ const toggleMenu = () => {
 
 <template>
     <header
-        class="fixed z-10 left-1/2 -translate-x-1/2 inline-flex justify-center items-center p-4 top-0 w-full max-w-mobile box-border pb-0 tablet:max-w-tablet laptop:max-w-laptop"
+        :class="`fixed z-10 left-1/2 -translate-x-1/2 flex justify-center items-center p-4 top-0 w-full max-w-mobile box-border pb-0 transition-all duration-300 tablet:max-w-tablet laptop:max-w-laptop`"
     >
         <div
-            :class="`flex justify-between items-center bg-jungle-700/80 p-4 relative transition-all w-full duration-300 backdrop-blur ${
-                isOpen ? 'rounded-t-2xl' : 'rounded-2xl'
-            } laptop:px-6`"
+            :class="`flex flex-col justify-between items-start bg-jungle-700/80 backdrop-blur w-full px-4 py-5 rounded-2xl ${isOpen ? 'gap-4' : 'gap-0'} transition-all duration-300 tablet:flex-row tablet:items-center`"
         >
-            <div @click="toggleMenu" class="flex tablet:hidden flex-col justify-center items-center">
-                <span
-                    :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 ${
-                        isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
-                    }`"
-                ></span>
-                <span
-                    :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 my-0.5 ${
-                        isOpen ? 'opacity-0' : ''
-                    }`"
-                ></span>
-                <span
-                    :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 ${
-                        isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
-                    }`"
-                ></span>
+            <div class="flex justify-between w-full">
+                <div @click="toggleMenu" class="flex tablet:hidden flex-col justify-center items-center">
+                    <span
+                        :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 ${
+                            isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
+                        }`"
+                    ></span>
+                    <span
+                        :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 my-0.5 ${
+                            isOpen ? 'opacity-0' : ''
+                        }`"
+                    ></span>
+                    <span
+                        :class="`w-6 bg-sand-100 h-0.5 block rounded transition-all duration-300 ${
+                            isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+                        }`"
+                    ></span>
+                </div>
+
+                <NuxtLink to="/"
+                    ><p class="font-bold text-sand-100 w-auto text-2xl tablet:text-3xl laptop:text-4xl">
+                        Simon Botté
+                    </p></NuxtLink
+                >
             </div>
-            <NuxtLink to="/"><p class="font-bold text-sand-100 text-4xl">Simon Botté</p></NuxtLink>
-            <nav
-                :class="`block left-0  absolute bottom-0 w-full p-4 rounded-b-2xl bg-jungle-700/80 transition-all  duration-300 tablet:relative tablet:block tablet:w-fit tablet:opacity-100 tablet:bg-transparent tablet:flex-row tablet:p-0 tablet:translate-y-0 tablet:bottom-auto tablet:pointer-events-auto${
-                    isOpen ? ' translate-y-full opacity-100' : ' pointer-events-none translate-y-3/4 opacity-0'
-                }`"
-            >
+            <nav :class="`shrink-0 w-auto transition-all duration-300 overflow-hidden ${isOpen ? 'opacity-100 pointer-events-auto translate-y-0 max-h-32' : 'opacity-0 pointer-events-none -translate-y-1/2 max-h-0'} tablet:opacity-100 tablet:pointer-events-auto tablet:translate-y-0 tablet:max-h-32` ">
                 <ul class="flex flex-col gap-4 tablet:flex-row">
                     <li><nuxt-link class="text-xl" to="/a-propos">À propos</nuxt-link></li>
                     <li><nuxt-link class="text-xl" to="/projets">Projets</nuxt-link></li>
